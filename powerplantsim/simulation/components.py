1b3d09ca8af8bfd5175eab4d1d77262573d51698
BOP PC <provers.0-pluses@icloud.com>
2025-02-23 21:56:38 +0000

    fixed return values

--- powerplantsim/simulation/components.py
+++ powerplantsim/simulation/components.py
@@ -1,19 +1,22 @@
 class Wellhead:
     def __init__(self):
         pass
-    def compute_steam_output(Wh_pressure, Wh_flow):
-        SteamFromWellHead = Wh_pressure * Wh_flow
-        return 0
+    def compute_steam_output(Wellhead_pressure, Wellhead_flow):
+        SteamFromWellHead = Wellhead_pressure * Wellhead_flow
+        return {
+            "Steam_from_wellhead": SteamFromWellHead
+        }
     
 class SteamSeperator:
     def __init__(self):
         pass
-    def compute_waste_water(SteamFromWellhead, waste_index, randomizer):
+    def compute_waste_water(SteamFromWellhead, waste_index):
+        waste_water_flow =  SteamFromWellhead * waste_index 
         return {
-        "waste_water_flow": SteamFromWellhead * waste_index * randomizer,
+            "waste_water_flow": waste_water_flow
         }
 
-    def waste_water_energy(waste_water_flow, Wh_pressure, steam_seperator_temperature):
+    def waste_water_energy(waste_water_flow, Wellhead_pressure, steam_seperator_temperature):
         waste_water_energy = waste_water_flow * steam_seperator_temperature                   #Finna function fyrir útreikning á varma í vatninu.
         return 1
     
