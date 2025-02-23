8a3ba2c917e302db61cdc9ec68fa6d32414b8033
BOP PC <provers.0-pluses@icloud.com>
2025-02-23 20:49:53 +0000

    added classes

---
+++ powerplantsim/simulation/components.py
@@ -0,0 +1,49 @@
+class Wellhead:
+    def __init__(self):
+        pass
+    def compute_steam_output(self):
+        # placeholder
+        return 0
+    
+class SteamSeperator:
+    def __init__(self):
+        pass
+    def compute_waste_water(self):
+        # placeholder
+        return 0
+
+class MoistureSeperator:
+    def __init__(self):
+        pass
+    def compute_waste_water(self):
+        # placeholder
+        return 0
+
+class SteamTurbine:
+    def __init__(self):
+        pass
+    def compute_mechanical_power_output(self):
+        # placeholder
+        return 0
+        
+class Generator:
+    def __init__(self):
+        pass
+    def compute_electric_power_output(self):
+        # placeholder
+        return 0
+    
+class Condenser:
+    def __init__(self):
+        pass
+    def compute_cooling_capacity(self):
+        # placeholder
+        return 0
+    
+class CoolingTower:
+    def __init__(self):
+        pass
+    def compute_water_cooling_capacity(self):
+        # placeholder
+        return 0
+            
