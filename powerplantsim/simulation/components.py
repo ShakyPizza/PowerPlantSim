73fea44fc19f646865ab77ef13df0d21d1105d2d
BOP PC <provers.0-pluses@icloud.com>
2025-02-23 21:49:14 +0000

    updates to components.py

--- powerplantsim/simulation/components.py
+++ powerplantsim/simulation/components.py
@@ -1,14 +1,26 @@
 class Wellhead:
     def __init__(self):
         pass
-    def compute_steam_output(self):
-        # placeholder
+    def compute_steam_output(Wh_pressure, Wh_flow):
+        SteamFromWellHead = Wh_pressure * Wh_flow
         return 0
     
 class SteamSeperator:
     def __init__(self):
         pass
-    def compute_waste_water(self):
+    def compute_waste_water(SteamFromWellhead, waste_index, randomizer):
+        waste_water_flow  = SteamFromWellhead * waste_index * randomizer
+        return 0
+
+    def waste_water_energy(waste_water_flow, Wh_pressure, steam_seperator_temperature):
+        waste_water_energy = waste_water_flow * steam_seperator_temperature                   #Finna function fyrir útreikning á varma í vatninu.
+        return 0
+    
+    def compute_steam_forward(SteamFromWellhead, waste_water_energy):
+        steam_forward = SteamFromWellhead - waste_water_energy
+        return 0
+    
+    def process(self):
         # placeholder
         return 0
 
@@ -16,6 +28,13 @@ class MoistureSeperator:
     def __init__(self):
         pass
     def compute_waste_water(self):
+        # placeholder
+        return 0
+
+class ReliefValve:
+    def __init__(self):
+        pass
+    def compute_valve_opening(self):
         # placeholder
         return 0
 
@@ -46,4 +65,5 @@ class CoolingTower:
     def compute_water_cooling_capacity(self):
         # placeholder
         return 0
+            
             
