d03a31a17e169b4056ea0c0865aa9523ffce97a0
Benedikt_MBP <provers.0-pluses@icloud.com>
2025-02-24 00:10:16 +0000

    fix typos in class names and update state variable names for clarity

--- powerplantsim/simulation/engine.py
+++ powerplantsim/simulation/engine.py
@@ -7,8 +7,8 @@ class SimulationEngine:
     def __init__(self):
         
         self.wellhead = Wellhead()
-        self.steamseparator = SteamSeperator()
-        self.moistureseperator = MoistureSeperator()
+        self.steamseparator = SteamSeparator()
+        self.moistureseparator = MoistureSeparator()
         self.turbine = SteamTurbine()
         self.condenser = Condenser()
         self.generator = Generator()
@@ -21,9 +21,11 @@ class SimulationEngine:
             "wellhead_temp": 186.1,      # °C
             "wellhead_flow": 180,      # kg/s
             "separator_pressure": None,
-            "steam_flow": None,
+            "separator_steam_flow": None,
+            "separator_steam_temp": None,
             "waste_water_flow": None,
-            "turbine_power": 0.0,
+            "turbine_out_power": 0.0,
+            "steam_flow": None,
             "condenser_pressure": 0.06,   # barA
             "condenser_temp": 35,      # °C
         }
@@ -35,19 +37,20 @@ class SimulationEngine:
 
         # 1) Update the separator with the wellhead values
         separator_result = self.steamseparator.process(
-            inlet_pressure=self.state["wellhead_pressure"],
-            inlet_temp=self.state["wellhead_temp"],
-            inlet_flow=self.state["wellhead_flow"]
+            separator_inlet_pressure=self.state["wellhead_pressure"],
+            separator_inlet_temp=self.state["wellhead_temp"],
+            separator_inlet_flow=self.state["wellhead_flow"]
         )
         self.state["separator_pressure"] = separator_result["separator_pressure"]
-        self.state["steam_flow"] = separator_result["steam_flow"]
-        # ... etc., store additional separator outputs into self.state
+        self.state["separator_steam_flow"] = separator_result["separator_steam_flow"]
+        self.state["separator_steam_temp"] = separator_result["separator_steam_temp"]
+        self.state["steam_flow"] = self.state["separator_steam_flow"]
 
         # 2) Update the turbine using the steam flow from the separator
         turbine_result = self.turbine.compute_mechanical_power_output(
-            inlet_pressure=self.state["separator_pressure"],
-            steam_flow=self.state["steam_flow"],
-            outlet_pressure=self.state["condenser_pressure"]
+            turbine_inlet_pressure=self.state["separator_pressure"],
+            turbine_inlet_steam_flow=self.state["separator_steam_flow"] / 4,
+            turbine_outlet_pressure=self.state["condenser_pressure"]
         )
         self.state["turbine_out_power"] = turbine_result["mechanical_power"]
         # ... store additional turbine outputs in self.state (mass flow out, etc.)
